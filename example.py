c9d890d82b8753bbaf06ddbfb9bed4d7d2210e1e
Mike Swirsky <mike.swirsky@gmail.com>
2019-09-13 02:09:30 -0700

    changed plot to seaborn

--- example.py
+++ example.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.DataFrame([[2,5,3],[1,8,4]])
-print(df)
+df = pd.DataFrame(columns=['A', 'B', 'C'])
 
+for col in df.columns:
+    df[col] = np.random.lognormal(size=1000)
+
+df['D'] = df['A'] * df['B']
 
 # Doing some math
 log_df = np.log(df)
-
 mult_df = log_df * 2
 
 # Printing output
+print('mult_df')
 print(mult_df.head())
+
+sns.scatterplot('C', 'D', data=mult_df)
+plt.title('Example Scatter Plot')
+plt.show(block=True)
