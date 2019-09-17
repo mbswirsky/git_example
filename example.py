77204e894e6d98081214095f8a336889a4d2e62b
Mike Swirsky <mike.swirsky@gmail.com>
2019-09-16 19:05:46 -0700

    fixed mult

--- example.py
+++ example.py
@@ -22,19 +22,13 @@ df['D'] = df['A'] * df['B']
 # Doing some math
 
 log_df = np.log(df)
-# mult_df = log_df * 2
+mult_df = log_df * 2
 
 # Printing output
 
 print('log_df')
 print(log_df.head())
 
-<<<<<<< HEAD
-sns.scatterplot('C', 'D', data=log_df)
-plt.title('Example Scatter Plot')
-plt.show()
-=======
 sns.scatterplot('C', 'D', data=mult_df)
 plt.title('Example Scatter Plot - Mult DF')
 plt.show() 
->>>>>>> test
